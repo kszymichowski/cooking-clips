bafd2e06ea6d86a9694224d0bbcec4c3955c6bb0
Konstanty Szymichowski <kostek.szymi@gmail.com>
2024-03-12 11:50:05 +0100

    add s3 storage option

--- cooking_clips/file_service/files.py
+++ cooking_clips/file_service/files.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
+import boto3
 
 class FileStorage(ABC):
     @abstractmethod
@@ -11,3 +12,11 @@ class LocalFileStorage(FileStorage):
     def upload_file(self, file_path: Path, file: bytes):
         with open(file_path, "wb") as buffer:
             buffer.write(file)
+
+class S3FileStorage(FileStorage):
+    def __init__(self, bucket_name: str):
+        self.bucket_name = bucket_name
+        self.s3_client = boto3.client('s3')
+
+    def upload_file(self, file_path: str, file: bytes):
+        self.s3_client.upload_fileobj(file, self.bucket_name, file_path)
